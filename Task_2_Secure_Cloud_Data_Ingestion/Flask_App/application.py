f9d275222ac9eeff1924959732da465099182636
Hammad Qureshi <124398224+m-hammad-qureshi@users.noreply.github.com>
2026-06-05 11:26:51 +0500

    Add files via upload

---
+++ Task_2_Secure_Cloud_Data_Ingestion/Flask_App/application.py
@@ -0,0 +1,202 @@
+""" To run the flask app use this command in terminal "flask --app main run", "python main.py run", but this will not refresh the app along with the changes you made. To refresh it with your code updating use debug mode like this "flask --app main run --debug" here main is your file name in my case the file name is main. Or you can just pass the debug=True like this app.run(debug=True). """
+
+""" Hashing using the python bcrypt module is a one way for verification, once the password is hashed you never back it in original form. """
+""" Cryptography is a two way of recovery like after encrypting the data you can also decrypt it again. """
+
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+from sqlalchemy import create_engine, text
+import bcrypt
+from cryptography.fernet import Fernet
+from dotenv import load_dotenv
+import os
+
+load_dotenv()   # Will load all the keys and sensitive data stored in the .env file.
+
+raw_key = os.getenv("ENCRYPTION_KEY")     # Encryption key for the email and phone.
+
+if raw_key:
+    encoded_key = raw_key.encode('utf-8')
+    cipher = Fernet(encoded_key)
+else:
+    raise ValueError("Encryption key not found in the .env file.")
+
+
+"""This function will encrypt the user data while entering the data of newly entered user at registration, it will encode the human language into the binary language for computer understanding, then it will encrypt this encoded text and in the end return to its decoded form to store into the database."""
+
+def encrypted_data(plain_text):
+    if not plain_text:
+        return None
+    plain_text_byte = plain_text.encode('utf-8')
+    encrypted_text = cipher.encrypt(plain_text_byte)
+    return encrypted_text.decode('utf-8')
+
+"""This function decrypt the user data will bring the data into its original form, with same functionality of encrypted function like encoding, decrypting and decoding."""
+
+def decrypted_data(cipher_text):
+    if not cipher_text:
+        return None
+    if isinstance(cipher_text, str):
+        cipher_text_byte = cipher_text.encode('utf-8')
+    else:
+        cipher_text_byte = cipher_text
+
+    decrypted_text = cipher.decrypt(cipher_text_byte)
+    return decrypted_text.decode('utf-8')
+
+
+base_url = os.getenv("DATABASE_URL")
+def db_insert():
+    """ 2. Extract the database name you want to use (e.g., 'user_auth_db') """
+    # db_name = base_url.split('/')[-1]
+    
+    """ 3. Connect to the server root level first to initialize the schema container safely """
+    # root_url = base_url.rsplit('/', 1)[0] + '/information_schema'
+    # root_engine = create_engine(root_url)
+    
+    # with root_engine.connect() as root_conn:
+    """ Create the custom database schema container if it doesn't exist yet """
+    #     root_conn.execute(text(f"CREATE DATABASE IF NOT EXISTS {db_name};"))
+    #     root_conn.commit()
+    # root_engine.dispose()
+
+    target_engine = create_engine(base_url, echo= True)    
+    with target_engine.connect() as conn:
+        conn.execute(text('''create table if not exists 
+                    users (id integer Primary key autoincrement,
+                        username varchar(100) NOT NULL, 
+                        email varchar(250) NOT NULL, 
+                        phone varchar(250) NOT NULL,
+                        password varchar(250) NOT NULL);'''))
+        conn.commit()
+    target_engine.dispose()    
+
+db_insert()
+
+engine = create_engine(base_url, echo=True)
+
+application = Flask(__name__)
+
+""" Here __name__ is acting like a map for the flask as this library is used to create web apps so we will need html for the text css for the style and some JS too. So, this special variable is used to navigate the right files need for the application. Upon running the file python automatically sets the __name__ to the value __main__. And __main__ is a string value assigned from the python. Assign the file with name and main as the first file as the parent file. """
+
+application.secret_key = os.getenv("APP_SESSION_KEY")
+
+@application.route('/')
+def root():
+    return redirect(url_for('login'))
+
+
+""" route is a decorator which tells the flask that which function will trigger, like here passed / in the route is where this app will land first and it will tell what to show the user with the help of this function. """
+
+@application.route('/Home')
+def home():
+    if 'username' in session:
+        logged_user = session["username"]
+        with engine.connect() as conn:
+            data_query = text("select * from users where username = :logged_user")
+            data_result = conn.execute(data_query, {
+                "logged_user": logged_user
+            }).fetchone()
+            if data_result:
+
+# """ The mapping method is used to properly assign a name of the column to flask app so it can't get distracted and bring the wrong data from the table. """
+                id_data = data_result._mapping['id']
+                username_data = data_result._mapping['username']
+                email_data = data_result._mapping['email']
+                email_data_decrypted = decrypted_data(email_data) # --> To look the decrypted email
+                phone_data = data_result._mapping['phone']
+                phone_data_decrypted = decrypted_data(phone_data) # --> To look the decrypted phone
+                hashed_pass = data_result._mapping['password']
+
+                return render_template('index.html', id = id_data,user = username_data, email = email_data, phone = phone_data, password = hashed_pass)
+            return redirect(url_for('login'))
+    else:
+        return redirect(url_for('login'))        
+
+@application.route('/login', methods = ['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username_input = request.form.get('username')
+        password_input = request.form.get('password')
+
+        with engine.connect() as conn:
+            login_query = text("select * from users where username = :username")
+            login_result = conn.execute(login_query, {
+                "username": username_input}).fetchone()
+        if login_result is None:
+            flash("The user is not exist")
+            return redirect(url_for('login'))
+        else:
+            stored_pass = login_result._mapping['password']
+            user_pass_byte = password_input.encode('utf-8')
+            stored_pass_byte = stored_pass.encode('utf-8')
+            if bcrypt.checkpw(user_pass_byte, stored_pass_byte):  
+                # --> checking if the hash-pass in the db and user given pass while login is matching or not.
+                print("Welcome back")
+                session["username"] = username_input
+                return redirect(url_for('home'))  
+            else:
+                flash("Invalid password, try again!")
+                return redirect(url_for('login'))  
+
+    return render_template('login.html')
+
+@application.route('/register', methods = ['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        username_input = request.form.get('username')
+        email_input = request.form.get('email')
+        phone_input = request.form.get('phone')
+        password_input = request.form.get('password')
+
+        email_encrypted = encrypted_data(email_input) # --> Encrypting the email using encrypted function
+        phone_encrypted = encrypted_data(phone_input) # --> Same encryption step for the phone data
+
+        with engine.connect() as conn:
+            query = text('select * from users where username = :username') # --> Parameterized query
+            result = conn.execute(query,{
+                "username": username_input}).fetchone()
+            
+            if result is not None:
+                flash("The username is already taken, try another one!")
+                return redirect((url_for('register')))
+            
+            all_rows = text("select email, phone from users")
+            all_rows_result = conn.execute(all_rows).fetchall()
+
+            duplicate_found = False
+
+            for row in all_rows_result:
+                if decrypted_data(row._mapping['email']) == email_input or decrypted_data(row._mapping['phone']) == phone_input:
+                    duplicate_found = True
+                    break
+
+            if duplicate_found:
+                flash("The email or phone is already exist, try new one!")
+                return redirect(url_for('register'))    
+
+            print("Adding a new user")
+            pass_bytes = password_input.encode('utf-8')
+            salt = bcrypt.gensalt()
+            hashed_password = bcrypt.hashpw(pass_bytes, salt)
+            stored_password = hashed_password.decode('utf8')
+
+            insert_query = text('''
+                            INSERT INTO users(username,email,phone,password) 
+                            VALUES
+                            (:username,:email,:phone,:password) ''')
+
+            insert_result = conn.execute(insert_query,{
+                "username": username_input,
+                "email": email_encrypted,
+                "phone": phone_encrypted,
+                "password": stored_password
+            })
+            conn.commit()
+
+            return redirect(url_for('login'))
+
+    return render_template('Register.html')
+
+
+if __name__ == '__main__':
+    application.run(host="0.0.0.0", port=5000)
